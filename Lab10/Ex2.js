8b4ca8a179f2d169ab92e061f5244183ef6f6c0e
deborahyuan <112019910+deborahyuan@users.noreply.github.com>
2022-09-30 19:52:46 -1000

    Lab10 move

--- Lab10/Ex2.js
+++
@@ -1,8 +0,0 @@
-const { split } = require("lodash");
-
-var attributes  =  "<name>;<age>;<age + 0.5>;<0.5 - age>";
-var pieces = attributes.split(";");
-
-for (i=0;i<pieces.length; i++) {
-    console.log(pieces[i]);
-}
