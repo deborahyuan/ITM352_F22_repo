eb8b653f197230adfadef367b57e66fd02b0c425
deborahyuan <112019910+deborahyuan@users.noreply.github.com>
2022-10-03 00:04:29 -1000

    SmartPhoneProducts3 WOD

---
+++ WODs/SmartPhoneProducts3/products_data.js
@@ -0,0 +1,7 @@
+var products = [
+    {'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg', 'brand':'HTC', 'price':40.00},
+    {'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg', 'brand':'Apple', 'price':75.00},
+    {'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg', 'brand':'Nokia', 'price':35.00},
+    {'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg','brand':'Samsung', 'price':45.00},
+    {'image': 'http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg', 'brand':'Blackberry', 'price':10},
+];
