5e18ec4f6fc05bf75f3e96e6c2c4da9b63aa59a7
deborahyuan <112019910+deborahyuan@users.noreply.github.com>
2022-10-14 22:32:03 -1000

    Lab12

--- Lab10/Ex3.js
+++ Lab10/Ex3.js
@@ -3,12 +3,7 @@ const { split } = require("lodash");
 // var attributes  =  "<name>;<age>;<major>";
 var attributes = "Deborah;21+0.5;21-0.5";
 
-var pieces = attributes.split(";");
-
-for (i=0; i<pieces.length; i++) {
-    console.log(`i=${i} value=${pieces[i]} type=${typeof pieces[i]}`);
-
-}
+forEeach()
 
 var piecesTogether = pieces.toString();
 
